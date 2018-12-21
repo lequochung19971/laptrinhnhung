2bd1e443044db84b4cccb7f34895de061da5874f
lequochung <43690592+lequochung19971@users.noreply.github.com>
2018-12-21 11:20:54 +0700

    Create database.js

---
+++ gamebtSave/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+ 'connection':{
+  'host':'embedded.cevumfc6keel.ap-southeast-1.rds.amazonaws.com',
+  'user':'root',
+  'password':'',
+  
+ },
+ 'database':'nodejs_login',
+  'user_table':'users'
+}
